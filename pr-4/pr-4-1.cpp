e4dffd62a9ef39b2a5554f22ccde941f7a1c0613
VasuVibingPrivetly <143987026+vasuvibingprivetly@users.noreply.github.com>
2023-12-25 19:26:00 +0530

    pr-4-1.cpp

--- pr-4/pr-4-1.cpp
+++ pr-4/pr-4-1.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 #include<string.h>
+using namespace std;
 
-class {
+class Message{
 	
+	private : 
+		string s = "hello";
+		string s2;
+		
+	public : 
+		Message(string s1){
+			s2 = s;
+			cout << s1;
+		}
+		
+		void getAdata(){
+			cout << " " << this->s << endl;
+			
+		}		
 };
+
+int main(){
+	char n[20];
+	
+	cout <<  "Enter string: ";
+	gets(n);
+	
+	Message obj1(n);
+	
+	obj1.getAdata();
+}
